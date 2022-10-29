55c8958ba260ba6ac2c69d468d34e82a3fa1f0f7
seon <seonsy44@gmail.com>
2022-10-29 23:55:49 +0900

    feat: 스타일 관련 코드 추가

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
+import GlobalStyles from './styles/GlobalStyles.js';
+import colors from './styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyles />
+    <ThemeProvider theme={colors}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/styles/theme.js
@@ -0,0 +1,8 @@
+const colors = {
+  backgroundColor: '#F6F8FA',
+  borderColor: '#D0D7DE',
+  pointColor: '#0969DA',
+  grayColor: '#848B92',
+};
+
+export default colors;
